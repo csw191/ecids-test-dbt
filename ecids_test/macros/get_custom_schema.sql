83efb96fe89767a8d4ec3ded6d87dbf7dd80cc4f
CSW191 <joshua.parep@mt.gov>
2025-08-07 13:01:58 -0600

    Updated ECIDS

---
+++ ecids_test/macros/get_custom_schema.sql
@@ -0,0 +1,14 @@
+-- FILE: macros/get_custom_schema.sql
+-- LOCATION: ~/child_assistance_analytics/macros/get_custom_schema.sql
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+  {%- set default_schema = target.schema -%}
+  
+  {%- if custom_schema_name is none -%}
+    {{ default_schema }}
+  {%- else -%}
+    {{ custom_schema_name | trim }}
+  {%- endif -%}
+
+{%- endmacro %}
